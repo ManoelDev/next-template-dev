51109e90a8a59c4ac4b54d246edd8480da144d84
Manoel Neto <maguserv@gmail.com>
2022-12-03 19:13:31 -0300

    refactor: pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
 
-module.exports = nextConfig;
-
-module.exports = withPWA({
+const withPWA = require('next-pwa')({
   dest: 'public',
+  register: true,
+  skipWaiting: true,
   disable: !isProd,
 });
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+});
+
+module.exports = nextConfig;
