19f359aca7952ce8393d665a17903bf8bde169e1
Manoel Neto <maguserv@gmail.com>
2022-12-03 17:26:45 -0300

    configure husky and lint-staged

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,7 +30,6 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
         </Head>
 
         <body>
