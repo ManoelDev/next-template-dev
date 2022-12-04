6beb3d0d39a9b94bf2782de5955f5a16be7265a8
Manoel Neto <maguserv@gmail.com>
2022-12-04 01:38:49 -0300

    chore: update nest config end format files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  skipWaiting: true,
-  disable: !isProd,
+  disable: process.env.NODE_ENV === 'development',
+  sw: '/sw.js',
 });
 
 const nextConfig = withPWA({
